aab74129994f12eab0421c2afb482645b6659702
Mashood ur Rehman Rana <ranamashood@proton.me>
2023-01-30 22:56:40 +0500

    update Button component style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,21 @@
-const Button = () => {
-  return <button>button</button>;
+import styled from "styled-components";
+
+interface Props {
+  title: string;
+}
+
+const Button = (props: Props) => {
+  const { title } = props;
+
+  return <StyledButton>{title}</StyledButton>;
 };
+
+const StyledButton = styled.button`
+  background-color: ${(props) => props.theme.colors.tertiary};
+  padding: 5px 10px;
+  border-radius: ${(props) => props.theme.borderRadius};
+  outline: none;
+  border: none;
+`;
 
 export default Button;
