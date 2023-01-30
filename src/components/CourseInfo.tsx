9c69f936dd72f5e0aaa87eb163f8f5430b222394
Mashood ur Rehman Rana <ranamashood@proton.me>
2023-01-31 02:26:55 +0500

    updated app layout

--- src/components/CourseInfo.tsx
+++ src/components/CourseInfo.tsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
+import courseInfo from "../data";
+import { CourseInfoInterface } from "../models";
 import Button from "./Button";
+import Select from "./Select";
 import TextInput from "./TextInput";
 
 const CourseInfo = () => {
+  const [course, setCourse] = useState<CourseInfoInterface>(courseInfo[0]);
+
   return (
     <div>
       <TextInput />
-      <TextInput />
-      <TextInput />
-      <Button />
+      <Select
+        options={courseInfo}
+        optionKey="grade"
+        value={course.grade}
+        setCourse={setCourse}
+      />
+      <Select
+        options={courseInfo}
+        optionKey="gpa"
+        value={course.gpa}
+        setCourse={setCourse}
+      />
+      <Button title="Add" />
     </div>
   );
 };
--- src/components/layout/Content.tsx
+++ src/components/layout/Content.tsx
@@ -1,11 +1,17 @@
+import styled from "styled-components";
 import CourseInfo from "../CourseInfo";
 
 const Content = () => {
   return (
-    <div>
+    <Wrapper>
       <CourseInfo />
-    </div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  background-color: ${(props) => props.theme.colors.primary};
+  height: 100vh;
+`;
 
 export default Content;
